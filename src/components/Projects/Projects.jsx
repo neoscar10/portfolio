8deb9425826ef8acc452ee7411cf384496465ee3
Neo <neoscar10@gmail.com>
2024-10-14 12:44:24 +0100

    Added attendance system

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,6 +3,7 @@ import viberr from '../../assets/viberr.png';
 import ProjectCard from '../../common/ProjectCard';
 import currencyConverter from "../../assets/currency_converter.png"
 import simonGame from "../../assets/simon-game.jpeg"
+import attendanceSyst from "../../assets/attendanceSystem.jpg"
 
 
 
@@ -11,11 +12,18 @@ function Projects() {
     <section id="projects" className={styles.container}>
      <h1 className='sectionTitle'>Projects</h1>
      <div className={styles.projectsContainer}>
+        
         <ProjectCard
           src={currencyConverter}
           link="https://github.com/neoscar10/Currency-converter-with-Nodejs"
           h3="Currency Converter"
           p="A currency converter using live data from an API"
+        />
+        <ProjectCard
+          src={attendanceSyst}
+          link="https://github.com/neoscar10/Django-RFID-based-attendance-system"
+          h3="Django-RFID Attendance SYStem"
+          p="An IoT project where A Micro-controller(ESP-8266) communicates and sends Attendance data to a Django backend via APIs"
         />
         <ProjectCard
           src={simonGame}
